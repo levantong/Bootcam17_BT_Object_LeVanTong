a33f26e407b431b1be0d71b44b7770b9b9dd9c07
levantong <vantong112.vpb@gmail.com>
2022-02-27 19:25:48 +0700

    Sửa độ cao cột textarea tên Modal
    Sửa độ cao cột textarea tên Modal

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 console.clear();
 
 //lấy lại danh sách sản phẩm từ local Storage
-if(localStorage.getItem("DSSP")){
+if (localStorage.getItem("DSSP")) {
     var danhSachSP = JSON.parse(localStorage.getItem("DSSP"));
 } else {
     var danhSachSP = [];
@@ -54,16 +54,21 @@ var renderListSP = function (dssp) {
     })
     document.getElementById("tblDanhSachSP").innerHTML = inner;
 }
-var hideEdit = function(){
-    document.getElementById("btnCapNhat").style.display = "none";
-}
-document.getElementById("btnThemSP").addEventListener("click",hideEdit);
 renderListSP(danhSachSP);
+
+//Xử lý ẩn nút Cập nhật, hiện nút thêm sản phẩm trên Modal khi bấm nút Thêm Mới
+var hideEdit = function () {
+    document.getElementById("btnCapNhat").style.display = "none";
+    document.getElementById("btnThemNV").style.display = "inline-block";
+
+}
+document.getElementById("btnThemSP").addEventListener("click", hideEdit);
+
 document.getElementById("btnThemNV").onclick = themSP;
 //hàm xóa sản phẩm
 var xoaSP = function (index) {
     danhSachSP.splice(index, 1);
-    alert(`Xóa sản phẩm thứ ${index+1} thành công!`);
+    alert(`Xóa sản phẩm thứ ${index + 1} thành công!`);
     renderListSP(danhSachSP);
     localStorage.setItem("DSSP", JSON.stringify(danhSachSP));
 
@@ -71,21 +76,20 @@ var xoaSP = function (index) {
 
 //hàm sửa sản phẩm
 var suaSP = function (index) {
+    //Xử lý hiện nút Cập nhật, ẩn nút thêm sản phẩm trên Modal khi bấm nút Sửa
+    document.getElementById("btnCapNhat").style.display = "inline-block";
+    document.getElementById("btnThemNV").style.display = "none";
     //hiện lại thông tin sp cần edit trên modal
     document.getElementById("TenSP").value = danhSachSP[index].ten;
     document.getElementById("GiaSP").value = danhSachSP[index].gia;
     document.getElementById("HinhSP").value = danhSachSP[index].hinh;
     document.getElementById("MoTa").value = danhSachSP[index].moTa;
-    //enabled lại nút Thêm Sản phẩm sau khi đã cập nhật thông tin
-    document.getElementById("btnCapNhat").style.display = "inline-block";
 
-
-    document.getElementById("btnThemNV").style.display = "none";
     //lưu index của sp cần sửa thông tin vào localStorage để dùng cho khi nhấn nút cập nhật sp (do giao diện đề không quản lý theo mã SP nên không dùng findIndex để tìm thứ tự sp cần edit)
-    localStorage.setItem("indexSPCanEdit",index)
+    localStorage.setItem("indexSPCanEdit", index)
 }
 
-var capNhatSP = function(){
+var capNhatSP = function () {
     //lấy thông tin nhập vào
     var TenSP = document.getElementById("TenSP").value;
     var GiaSP = document.getElementById("GiaSP").value;
@@ -110,28 +114,26 @@ var capNhatSP = function(){
     //tắt modal
     document.getElementById("btnDong").click();
     //thông báo thành công
-    alert(`Thay đổi thông tin sản phẩm thứ ${index+1} thành công!`);
-    //enabled lại nút Thêm Sản phẩm sau khi đã cập nhật thông tin
-    document.getElementById("btnThemNV").disabled = false;
+    alert(`Thay đổi thông tin sản phẩm thứ ${index + 1} thành công!`);
 }
 document.getElementById("btnCapNhat").onclick = capNhatSP;
 
 //enabled lại nút Thêm Sản phẩm khi nhấn thêm mới SP
-document.getElementById("btnThemSP").onclick = function(){
+document.getElementById("btnThemSP").onclick = function () {
     document.getElementById("btnThemNV").disabled = false;
 }
 //hàm loại bỏ dấu tiếng việt, tham khảo tại: https://www.tunglt.com/2018/11/bo-dau-tieng-viet-javascript-es6/
 function removeAccents(str) {
     return str.normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .replace(/đ/g, 'd').replace(/Đ/g, 'D');
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/đ/g, 'd').replace(/Đ/g, 'D');
 }
-var timSP = function(){
+var timSP = function () {
     //lấy thông tin nhập vào
     var inputTK = document.getElementById("inputTK").value;
     var keySearch = removeAccents(inputTK.toLowerCase().replace(/\s+/g, '')); //loại bỏ dấu TV và dấu cách, đổi sang chữ thường.
     //tạo mảng đã lọc theo từ khóa tìm kiếm, show mảng mới
-    var filterSP = danhSachSP.filter(function(sp){
+    var filterSP = danhSachSP.filter(function (sp) {
         return removeAccents(sp.ten.toLowerCase().replace(/\s+/g, '')).includes(keySearch)
     });
     renderListSP(filterSP);
